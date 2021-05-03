b4f79682c1feccfcfb15a370f17cb8710ac492d0
tairhadad <tairhadad1@gmail.com>
2021-05-03 18:09:18 +0300

    project

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './App.css'
 import TodoList from './components/TodoList'
 import Header from './components/Header'
+
 function App() {
   return (
     <div className="todo-app">
